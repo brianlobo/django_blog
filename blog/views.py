a077434bda10c10119be72cf6409d3714a2422e2
Brian Lobo <brianlobo.code@gmail.com>
2018-12-09 18:52:06 -0500

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Brian',
+        'title': 'Post 1',
+        'content': 'First Post data',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Kiara',
+        'title': 'Post 2',
+        'content': 'First Post data',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', { 'title': 'About'})
