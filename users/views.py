7754917415e1194a12c73cf59db77e4c9a850369
Brian Lobo <brianlobo.code@gmail.com>
2018-12-20 08:48:39 -0500

    Changed message when creating account

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Your account has been created! You can now login.')
+            messages.success(request, f'{username}, account has been created! You can now login.')
             return redirect('login')
     else:
         form = UserRegisterForm()
