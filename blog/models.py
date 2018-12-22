014e57421ddc4578e655271703b5144d5012c331
Brian Lobo <brianlobo.code@gmail.com>
2018-12-22 00:32:42 -0500

    Added the ability for a user to create new post, and update and delete post if they are the post's author. Added it to templates as well.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # This will be where you will redirected to after we create a post
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,97 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+    )
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 from django.shortcuts import render
 from .models import Post
+
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+
+    # <app>/<model>_<viewtype>.html
+    # blog/post_list.html <-Default ()
+    template_name = 'blog/home.html' 
+    # This will override the default location 
+    # it searches for a tempate
+
+    # By default, ListView will call 
+    # context by the name object_list
+    context_object_name = 'posts'   
+    # This overrides it
+
+    ordering = ['-date_posted'] 
+     # '-' makes it from newest to oldest
+    # 'date_post' from oldest to newest
+
+class PostDetailView(DetailView):
+    model = Post
+    # We wont set the 'template_name' so it will look in:
+    # blog/post_detail.html
+    # # <app>/<model>_<viewtype>.html
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # LoginRequiredMixin wont allow you to access post/new unless 
+    # you are logged in.
+    # Will redirect to login page.
+    model = Post
+    # Sets the fields for the post
+    fields = ['title', 'content']
+
+    # Requires an author so we override it to make it the user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # LoginRequiredMixin wont allow you to access post/new unless 
+    #      you are logged in. Will redirect to login page.
+    # UserPassesTestMixin will be used with test_func function
+    model = Post
+    # Sets the fields for the post
+    fields = ['title', 'content']
+
+    # Requires an author so we override it to make it the user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        # Gets post
+        post = self.get_object()
+        # Checks if user is tring to edit own post
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    # LoginRequiredMixin wont allow you to access post/new unless 
+    #      you are logged in. Will redirect to login page.
+    # UserPassesTestMixin will be used with test_func function
+    model = Post
+    # Will redirect home after
+    success_url = "/"
+
+    def test_func(self):
+        # Gets post
+        post = self.get_object()
+        # Checks if user is tring to edit own post
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', { 'title': 'About'})
