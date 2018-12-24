4182d5448f537881722b29a017d515424a078b1e
Brian Lobo <brianlobo.code@gmail.com>
2018-12-24 18:41:33 -0500

    Started building out a new front end. On the server side, I truncated the posts to show a max of 40 words

--- blog/views.py
+++ blog/views.py
@@ -37,13 +37,13 @@ class PostListView(ListView):
      # '-' makes it from newest to oldest
     # 'date_post' from oldest to newest
 
-    paginate_by = 4
+    paginate_by = 3
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html' 
     context_object_name = 'posts'   
-    paginate_by = 4
+    paginate_by = 3
 
     def get_queryset(self):
         # 404 if doesnt exist
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
@@ -30,7 +31,7 @@ urlpatterns = [
         ),
 ]
 
-
 # AND THIS
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
